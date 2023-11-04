09ece1e1b92ffcf034787626aa5a42a3d0d0aea0
Barbedi <146706805+Barbedi@users.noreply.github.com>
2023-11-05 00:56:37 +0100

    Metody

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -1,8 +1,44 @@
 ﻿#include <iostream>
-
 using namespace std;
-
-int main()
+struct node
 {
-    cout << "Hello World!\n";
+    node* lewo, * prawo;
+    int klucz;
+};
+
+class drzewo
+{
+private:
+    node* korzen;
+    unsigned counter;
+
+public:
+    void DodajElemntDoDrzewa(int klucz);
+    void WyswietlDrzewo(node* korzen);
+    void UsunElemntZDrzewa(int klucz);
+    void UsunDrzewo(node* korzen);
+    void SzukajDrogiDoPodanegoElemntu(node* korzen, int klucz);
+
+
+
+};
+
+void drzewo::DodajElemntDoDrzewa(int klucz)
+{
+}
+
+void drzewo::WyswietlDrzewo(node* korzen)
+{
+}
+
+void drzewo::UsunElemntZDrzewa(int klucz)
+{
+}
+
+void drzewo::UsunDrzewo(node* korzen)
+{
+}
+
+void drzewo::SzukajDrogiDoPodanegoElemntu(node* korzen, int klucz)
+{
 }
