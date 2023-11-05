7bfd99e190c1a8e5f211afa8d1eecd35a2cc72d1
Paweł <pawelporemba123@gmail.com>
2023-11-05 14:18:15 +0100

    Dodaj naglowek BST i main

---
+++ BST/BST.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef drzewo_H
+#define drzewo_H
+
+struct node
+{
+	node* lewo, * prawo;
+	int klucz;
+};
+
+class drzewo {
+private:
+	node* korzen;
+	unsigned counter;
+public:
+	void DodajElemntDoDrzewa(int klucz);
+	void WyswietlDrzewo(node* korzen);
+	void UsunElemntZDrzewa(int klucz);
+	void UsunDrzewo(node* korzen);
+	void SzukajDrogiDoPodanegoElemntu(node* korzen, int klucz);
+};
+#endif // !drzewo
---
+++ BST/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "BST.h";
+
+using namespace std;
+
+int main() {
+	cout << "Hello world!";
+}
